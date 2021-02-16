3e8f860b4a174280e08f040c0b78f994a12bb5d9
amir_moghadam <amoghadam@kilid.com>
2021-02-17 00:52:03 +0330

    reverting changes on loss functions

--- train.py
+++ train.py
@@ -784,7 +784,7 @@ def rpn_loss_regr(num_anchors):
     Returns:
         Smooth L1 loss function
                            0.5*x*x (if x_abs < 1)
-                           x_abs - 0.5 (otherwise)
+                           x_abx - 0.5 (otherwise)
     """
     def rpn_loss_regr_fixed_num(y_true, y_pred):
 
@@ -795,11 +795,10 @@ def rpn_loss_regr(num_anchors):
         x_abs = K.abs(x)
 
         # If x_abs <= 1.0, x_bool = 1
-        x_bool = K.cast(K.less_equal(x_abs, 1), tf.float32)
+        x_bool = K.cast(K.less_equal(x_abs, 1.0), tf.float32)
 
-        return lambda_rpn_regr * K.sum(
-            y_true[:, :, :, :4 * num_anchors] * (x_bool * (0.5 * x * x) +
-                                                 (1 - x_bool) * (x_abs - 0.5))) / \
+        return lambda_rpn_regr * K.sum(y_true[:, :, :, :4 * num_anchors] *
+                                       (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / \
                K.sum(epsilon + y_true[:, :, :, :4 * num_anchors])
 
     return rpn_loss_regr_fixed_num
@@ -817,9 +816,9 @@ def rpn_loss_cls(num_anchors):
     """
     def rpn_loss_cls_fixed_num(y_true, y_pred):
 
-            return lambda_rpn_class * K.sum(y_true[:, :, :, :num_anchors] *
-                                            K.binary_crossentropy(y_pred[:, :, :, :],
-                                                                  y_true[:, :, :, num_anchors:])) / \
+            return lambda_rpn_class * \
+                   K.sum(y_true[:, :, :, :num_anchors] *
+                         K.binary_crossentropy(y_pred[:, :, :, :], y_true[:, :, :, num_anchors:])) / \
                    K.sum(epsilon + y_true[:, :, :, :num_anchors])
 
     return rpn_loss_cls_fixed_num
@@ -837,8 +836,7 @@ def class_loss_regr(num_classes):
     def class_loss_regr_fixed_num(y_true, y_pred):
         x = y_true[:, :, 4*num_classes:] - y_pred
         x_abs = K.abs(x)
-        x_bool = K.cast(K.less_equal(x_abs, 1), 'float32')
-
+        x_bool = K.cast(K.less_equal(x_abs, 1.0), 'float32')
         return lambda_cls_regr * K.sum(y_true[:, :, :4*num_classes] *
                                        (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / \
                K.sum(epsilon + y_true[:, :, :4*num_classes])
