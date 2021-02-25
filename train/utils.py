3068d731f3d1fec45fc9c675f645b1be088feda4
amir_moghadam <amoghadam@kilid.com>
2021-02-25 13:27:02 +0330

    minor bug fixed in importing modules from utils

--- train/utils.py
+++ train/utils.py
@@ -3,11 +3,10 @@ import random
 import math
 import copy
 
-
-# Calculate IoU (Intersection of Union)
 from train.data import get_new_img_size
 
 
+# Calculate IoU (Intersection of Union)
 def union(au, bu, area_intersection):
     area_a = (au[2] - au[0]) * (au[3] - au[1])
     area_b = (bu[2] - bu[0]) * (bu[3] - bu[1])
