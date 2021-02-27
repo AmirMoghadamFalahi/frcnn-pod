7db673d371fa3eeb84c0e1aaf847e8173e22bac2
amir_moghadam <amoghadam@kilid.com>
2021-02-28 00:14:55 +0330

    Format image for mAP method added

--- train/data.py
+++ train/data.py
@@ -306,3 +306,45 @@ def get_real_coordinates(ratio, x1, y1, x2, y2):
     real_y2 = int(round(y2 // ratio))
 
     return (real_x1, real_y1, real_x2, real_y2)
+
+
+def format_img_map(img, C):
+    """Format image for mAP. Resize original image to C.im_size (300 in here)
+
+    Args:
+        img: cv2 image
+        C: config
+
+    Returns:
+        img: Scaled and normalized image with expanding dimension
+        fx: ratio for width scaling
+        fy: ratio for height scaling
+    """
+
+    img_min_side = float(C.im_size)
+    (height, width, _) = img.shape
+
+    if width <= height:
+        f = img_min_side / width
+        new_height = int(f * height)
+        new_width = int(img_min_side)
+    else:
+        f = img_min_side / height
+        new_width = int(f * width)
+        new_height = int(img_min_side)
+    fx = width / float(new_width)
+    fy = height / float(new_height)
+    img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+    # Change image channel from BGR to RGB
+    img = img[:, :, (2, 1, 0)]
+    img = img.astype(np.float32)
+    img[:, :, 0] -= C.img_channel_mean[0]
+    img[:, :, 1] -= C.img_channel_mean[1]
+    img[:, :, 2] -= C.img_channel_mean[2]
+    img /= C.img_scaling_factor
+    # Change img shape from (height, width, channel) to (channel, height, width)
+    img = np.transpose(img, (2, 0, 1))
+    # Expand one dimension at axis 0
+    # img shape becames (1, channel, height, width)
+    img = np.expand_dims(img, axis=0)
+    return img, fx, fy
